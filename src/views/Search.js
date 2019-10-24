37c22914eaa6dffd3a6473796f99db27f1d309fe
Guillem Moreso <guillem.moreso@gmail.com>
2019-10-24 19:06:03 +0200

    handlechanges in Search view updated

--- src/views/Search.js
+++ src/views/Search.js
@@ -5,12 +5,20 @@ import DatePicker from 'react-date-picker';
 class Search extends Component {
   state = {
     date: new Date(),
-    value: '22:00',
+    startingHour: '22:00',
   };
 
-  onChange = date => this.setState({ date });
+  onDateChange = date => {
+    console.log('dateeeee', date);
+    this.setState({ date });
+  };
 
-  handleChange = event => this.setState({ value: event.target.value });
+  onHourChange = hour => {
+    console.log('houuuur', hour.target.value);
+    this.setState({ hour });
+  };
+
+  handleChange = event => this.setState({ startingHour: event.target.value });
 
   handleFormSubmit = e => {
     e.preventDefault();
@@ -22,13 +30,13 @@ class Search extends Component {
         <h1>Search here your next game:</h1>
         Select date:
         <br />
-        <DatePicker onChange={this.onChange} value={this.state.date} />
+        <DatePicker onChange={this.onDateChange} value={this.state.date} />
         <br />
         <form onSubmit={this.handleFormSubmit}>
           <label>
             Select starting hour:
             <br />
-            <select>
+            <select onChange={this.onHourChange}>
               <option value="20:00">20:00</option>
               <option value="21:00">21:00</option>
               <option selected value="22:00">
