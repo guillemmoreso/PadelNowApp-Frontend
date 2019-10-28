d35e72c7ea5ae586b60eac8a1331394283a77029
Guillem Moreso <guillem.moreso@gmail.com>
2019-10-28 20:01:17 +0100

    wip restyling booking page

--- src/views/Bookings.js
+++ src/views/Bookings.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { withAuth } from '../Context/AuthContext';
 import bookingsService from '../services/bookingsService';
 import Loading from '../components/Loading/Loading';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 
 class Bookings extends Component {
   state = {
@@ -29,20 +31,32 @@ class Bookings extends Component {
         <header className="header-bookings">
           <h1>Bookings</h1>
         </header>
-        <section className="card-container">
-          <div className="header">
-            <p className="title">Bookings</p>
-            {!isLoading &&
-              bookings.map(booking => {
-                return (
-                  <div key={booking._id}>
-                    <p>{booking.startingHour}</p>
+        {bookings.length > 0 ? (
+          <>
+            {bookings.map(booking => {
+              return (
+                <div id="booking-card" key={booking._id}>
+                  <div id="moment-booking">
+                    <Moment format="DD/MM HH"></Moment>
                   </div>
-                );
-              })}
-            {isLoading && <Loading />}
-          </div>
-        </section>
+
+                  <p>{booking.startingHour}</p>
+                  <p>{booking.day}</p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <h1>You have not booked a match yet... </h1>
+            <Loading />
+
+            <Link id="logout-btn-div" to="/search">
+              {/* <Link id="home-book-btn-div" to={`/clubs/${club._id}`}> */}
+              <div id="logout-btn">Book now</div>
+            </Link>
+          </>
+        )}
       </>
     );
   }
