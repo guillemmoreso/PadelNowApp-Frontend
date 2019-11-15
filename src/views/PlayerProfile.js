96be432d7277994d7031f6182c2161468794a8f9
Guillem Moreso <guillem.moreso@gmail.com>
2019-11-15 20:01:00 +0100

    stats profile WIP

--- src/views/PlayerProfile.js
+++ src/views/PlayerProfile.js
@@ -12,7 +12,7 @@ class PlayerProfile extends Component {
   };
 
   render() {
-    const { name, surname, username } = this.props.user;
+    const { name, surname, username, avatarImg, description, level } = this.props.user;
 
     return (
       <>
@@ -20,9 +20,30 @@ class PlayerProfile extends Component {
           <Backbar history={this.props.history} />
           <h1>Edit Player Profile</h1>
         </div>
-        
-  
-
+        <div id="profile-stats-card">
+          <img id="user-profile-stats" src={avatarImg} alt="profile" />
+          <div id="profile-stats">
+            <p>
+              <span>Games</span>
+              <br />0
+            </p>
+            <p>
+              <span>Won</span>
+              <br />0
+            </p>
+            <p>
+              <span>Lost</span>
+              <br />0
+            </p>
+          </div>
+          <h2 style={{ margin: '20px 0 10px 20px' }}>About</h2>
+          <p>{description}</p>
+          <h2 style={{ margin: '20px 0 10px 20px' }}>Level</h2>
+          <p>{level}</p>
+        </div>
+        <div id="submit-reservation">
+          <input type="submit" value="Edit Stats" onClick={this.handleBookingDelete} id="submit-datapicker" />
+        </div>
       </>
     );
   }
